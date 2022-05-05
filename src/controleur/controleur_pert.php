1156da0cc90bf05cd74d6f9abeb36b91f6201352
Maxence <maxence.maziere@epsi.fr>
2022-05-05 19:50:48 +0200

    modifs

--- src/controleur/controleur_pert.php
+++ src/controleur/controleur_pert.php
@@ -20,134 +20,133 @@ function actionPert($twig, $db) {
     else{
       
     
-    foreach($listeTaches as $key=>$tache){
-        $id_tache_to_key[$tache['id_tache']] = $key;
-        $listeTachePreced = [];
-        $listeTacheSuivante = [];
-        $finalTable[$tache['id_tache']] = array();
-        $finalTable[$tache['id_tache']]['id_tache'] = $tache['id_tache'];
-        $finalTable[$tache['id_tache']]['libelle'] = $tache['libelle'];
-        $finalTable[$tache['id_tache']]['duree'] = $tache['duree'];
-        foreach($pert->selectTachesPrecedByTache($db,$tache['id_tache'],$not_in) as $tache_precedente){
-            array_push($listeTachePreced,$tache_precedente['id_tache_precedente']);
-        }
-        $finalTable[$tache['id_tache']]['liste_taches_precedentes'] = $listeTachePreced;
-        foreach($pert->selectTachesSuivantesByTache($tache['id_tache']) as $tache_suivante){
-            array_push($listeTacheSuivante,$tache_suivante['id_tache']);
-        }
-        $finalTable[$tache['id_tache']]['liste_taches_suivantes'] = $listeTacheSuivante;
-    }
-
-    $tache_fin = array("id_tache"=> -1,"duree"=>null,"liste_taches_precedentes"=>array(),"liste_tache_suivantes" => null);
-
-    // Fonction pour avoir les niveaux
-    while(count($listeTaches) != 0){
-        $tableTmpNiveau = [];
-        $not_in_tmp = "";
         foreach($listeTaches as $key=>$tache){
-            if(count($pert->selectTachesPrecedByTache($db,$tache['id_tache'],$not_in)) == 0){
-                $finalTable[$tache['id_tache']]['niveau'] = count($tableNiveau);
-                array_push($tableTmpNiveau,$tache['id_tache']);
-                $not_in_tmp .= ','.$tache['id_tache'];
-                unset($listeTaches[$key]);
-                continue;
+            $id_tache_to_key[$tache['id_tache']] = $key;
+            $listeTachePreced = [];
+            $listeTacheSuivante = [];
+            $finalTable[$tache['id_tache']] = array();
+            $finalTable[$tache['id_tache']]['id_tache'] = $tache['id_tache'];
+            $finalTable[$tache['id_tache']]['libelle'] = $tache['libelle'];
+            $finalTable[$tache['id_tache']]['duree'] = $tache['duree'];
+            foreach($pert->selectTachesPrecedByTache($db,$tache['id_tache'],$not_in) as $tache_precedente){
+                array_push($listeTachePreced,$tache_precedente['id_tache_precedente']);
+            }
+            $finalTable[$tache['id_tache']]['liste_taches_precedentes'] = $listeTachePreced;
+            foreach($pert->selectTachesSuivantesByTache($tache['id_tache']) as $tache_suivante){
+                array_push($listeTacheSuivante,$tache_suivante['id_tache']);
+            }
+            $finalTable[$tache['id_tache']]['liste_taches_suivantes'] = $listeTacheSuivante;
+        }
+
+        $tache_fin = array("id_tache"=> -1,"duree"=>null,"liste_taches_precedentes"=>array(),"liste_tache_suivantes" => null);
+
+        // Fonction pour avoir les niveaux
+        while(count($listeTaches) != 0){
+            $tableTmpNiveau = [];
+            $not_in_tmp = "";
+            foreach($listeTaches as $key=>$tache){
+                if(count($pert->selectTachesPrecedByTache($db,$tache['id_tache'],$not_in)) == 0){
+                    $finalTable[$tache['id_tache']]['niveau'] = count($tableNiveau);
+                    array_push($tableTmpNiveau,$tache['id_tache']);
+                    $not_in_tmp .= ','.$tache['id_tache'];
+                    unset($listeTaches[$key]);
+                    continue;
+                }
+            }
+            $not_in .= $not_in_tmp;
+            array_push($tableNiveau,$tableTmpNiveau);
+        }
+
+        $tache_fin['niveau'] = count($tableNiveau);
+
+        foreach($finalTable as $tache){
+            if(count($tache['liste_taches_suivantes']) == 0){
+                array_push($tache_fin['liste_taches_precedentes'],$tache['id_tache']);
             }
         }
-        $not_in .= $not_in_tmp;
-        array_push($tableNiveau,$tableTmpNiveau);
-    }
 
-    $tache_fin['niveau'] = count($tableNiveau);
+        $listeTaches = $listeTachesCopie;
 
-    foreach($finalTable as $tache){
-        if(count($tache['liste_taches_suivantes']) == 0){
-            array_push($tache_fin['liste_taches_precedentes'],$tache['id_tache']);
-        }
-    }
-
-    $listeTaches = $listeTachesCopie;
-
-    // Fonction pour avoir la durée min
-    while(count($listeTaches) != 0){
-        foreach($finalTable as $tache){
-            if($tache['niveau'] == 0){
-                $finalTable[$tache['id_tache']]['duree_min'] = 0;
-                unset($listeTaches[$id_tache_to_key[$tache['id_tache']]]);
-            }else{
-                $break = 0;
-                $liste_durees_prec = array();
-                foreach($tache['liste_taches_precedentes'] as $id_tache_precedente){
-                    if(isset($finalTable[$id_tache_precedente]['duree_min'])){
-                        array_push($liste_durees_prec,$finalTable[$id_tache_precedente]['duree_min']+$finalTable[$id_tache_precedente]['duree']);
-                    }else{
-                        $break = 1;
-                        break;
+        // Fonction pour avoir la durée min
+        while(count($listeTaches) != 0){
+            foreach($finalTable as $tache){
+                if($tache['niveau'] == 0){
+                    $finalTable[$tache['id_tache']]['duree_min'] = 0;
+                    unset($listeTaches[$id_tache_to_key[$tache['id_tache']]]);
+                }else{
+                    $break = 0;
+                    $liste_durees_prec = array();
+                    foreach($tache['liste_taches_precedentes'] as $id_tache_precedente){
+                        if(isset($finalTable[$id_tache_precedente]['duree_min'])){
+                            array_push($liste_durees_prec,$finalTable[$id_tache_precedente]['duree_min']+$finalTable[$id_tache_precedente]['duree']);
+                        }else{
+                            $break = 1;
+                            break;
+                        }
+                    }
+                    if($break == 0){
+                        $finalTable[$tache['id_tache']]['duree_min'] = max($liste_durees_prec);
+                        unset($listeTaches[$id_tache_to_key[$tache['id_tache']]]);
                     }
                 }
-                if($break == 0){
-                    $finalTable[$tache['id_tache']]['duree_min'] = max($liste_durees_prec);
+            }
+        }
+        $liste_durees_prec = array();
+        foreach($tache_fin['liste_taches_precedentes'] as $id_tache_precedente){
+            array_push($liste_durees_prec,$finalTable[$id_tache_precedente]['duree_min']+$finalTable[$id_tache_precedente]['duree']);
+        }
+
+        $tache_fin['duree_min'] =  max($liste_durees_prec);
+        $tache_fin['duree_max'] =  max($liste_durees_prec);
+
+        $listeTaches = $listeTachesCopie;
+
+        // Fonction pour avoir la durée max
+        while(count($listeTaches) != 0){
+            foreach($finalTable as $tache){
+                if(in_array($tache['id_tache'],$tache_fin['liste_taches_precedentes'])){
+                    $finalTable[$tache['id_tache']]['duree_max'] = $tache_fin['duree_max']  - $finalTable[$tache['id_tache']]['duree'];
                     unset($listeTaches[$id_tache_to_key[$tache['id_tache']]]);
+                }else{
+                    $break = 0;
+                    $liste_durees_suiv = array();
+                    foreach($tache['liste_taches_suivantes'] as $id_tache_suivante){
+                        if(isset($finalTable[$id_tache_suivante]['duree_max'])){
+                            array_push($liste_durees_suiv,$finalTable[$id_tache_suivante]['duree_max']);
+                        }else{
+                            $break = 1;
+                            break;
+                        }
+                    }
+                    if($break == 0){
+                        $finalTable[$tache['id_tache']]['duree_max'] = min($liste_durees_suiv) - $finalTable[$tache['id_tache']]['duree'];
+                        unset($listeTaches[$id_tache_to_key[$tache['id_tache']]]);
+                    }
                 }
             }
         }
-    }
-    $liste_durees_prec = array();
-    foreach($tache_fin['liste_taches_precedentes'] as $id_tache_precedente){
-        array_push($liste_durees_prec,$finalTable[$id_tache_precedente]['duree_min']+$finalTable[$id_tache_precedente]['duree']);
-    }
 
-    $tache_fin['duree_min'] =  max($liste_durees_prec);
-    $tache_fin['duree_max'] =  max($liste_durees_prec);
+        // Fonction pour avoir la marge totale
 
-    $listeTaches = $listeTachesCopie;
+        foreach($finalTable as $tache){
+            $finalTable[$tache['id_tache']]['marge_totale'] =  $finalTable[$tache['id_tache']]['duree_max'] - $finalTable[$tache['id_tache']]['duree_min'];
+        }
 
-    // Fonction pour avoir la durée max
-    while(count($listeTaches) != 0){
+        // Fonction pour avoir la marge libre
         foreach($finalTable as $tache){
             if(in_array($tache['id_tache'],$tache_fin['liste_taches_precedentes'])){
-                $finalTable[$tache['id_tache']]['duree_max'] = $tache_fin['duree_max']  - $finalTable[$tache['id_tache']]['duree'];
-                unset($listeTaches[$id_tache_to_key[$tache['id_tache']]]);
+                $finalTable[$tache['id_tache']]['marge_libre'] = $tache_fin['duree_min']  - $finalTable[$tache['id_tache']]['duree'] - $finalTable[$tache['id_tache']]['duree_min'];
+                $finalTable[$tache['id_tache']]['critique'] = $finalTable[$tache['id_tache']]['marge_libre'] == 0;
             }else{
-                $break = 0;
                 $liste_durees_suiv = array();
                 foreach($tache['liste_taches_suivantes'] as $id_tache_suivante){
-                    if(isset($finalTable[$id_tache_suivante]['duree_max'])){
-                        array_push($liste_durees_suiv,$finalTable[$id_tache_suivante]['duree_max']);
-                    }else{
-                        $break = 1;
-                        break;
-                    }
+                    array_push($liste_durees_suiv,$finalTable[$id_tache_suivante]['duree_min']);
                 }
-                if($break == 0){
-                    $finalTable[$tache['id_tache']]['duree_max'] = min($liste_durees_suiv) - $finalTable[$tache['id_tache']]['duree'];
-                    unset($listeTaches[$id_tache_to_key[$tache['id_tache']]]);
-                }
+                $finalTable[$tache['id_tache']]['marge_libre'] = min($liste_durees_suiv) - $finalTable[$tache['id_tache']]['duree'] - $finalTable[$tache['id_tache']]['duree_min'];
             }
         }
-    }
 
-    // Fonction pour avoir la marge totale
-
-    foreach($finalTable as $tache){
-        $finalTable[$tache['id_tache']]['marge_totale'] =  $finalTable[$tache['id_tache']]['duree_max'] - $finalTable[$tache['id_tache']]['duree_min'];
-    }
-
-    // Fonction pour avoir la marge libre
-    foreach($finalTable as $tache){
-        if(in_array($tache['id_tache'],$tache_fin['liste_taches_precedentes'])){
-            $finalTable[$tache['id_tache']]['marge_libre'] = $tache_fin['duree_min']  - $finalTable[$tache['id_tache']]['duree'] - $finalTable[$tache['id_tache']]['duree_min'];
-            $finalTable[$tache['id_tache']]['critique'] = $finalTable[$tache['id_tache']]['marge_libre'] == 0;
-        }else{
-            $liste_durees_suiv = array();
-            foreach($tache['liste_taches_suivantes'] as $id_tache_suivante){
-                array_push($liste_durees_suiv,$finalTable[$id_tache_suivante]['duree_min']);
-            }
-            $finalTable[$tache['id_tache']]['marge_libre'] = min($liste_durees_suiv) - $finalTable[$tache['id_tache']]['duree'] - $finalTable[$tache['id_tache']]['duree_min'];
-        }
-    }
-
-    array_push($finalTable,$tache_fin);
-    var_dump($finalTable);
+        array_push($finalTable,$tache_fin);
     }
 
 
@@ -207,7 +206,6 @@ function actionPert($twig, $db) {
         }
         echo json_encode($finalTableAPI);
     }else{
-        var_dump($finalTable);
         echo $twig->render('pert.html.twig', array('form'=>$form, 'taches'=>$finalTable));
     }
 }
